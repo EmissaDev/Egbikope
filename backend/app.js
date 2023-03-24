741ffa8e6174d26d6afa7335f2f883b4232bed4d
Emiline <emissadev@gmail.com>
2023-03-24 04:54:46 -0700

    Fixing page not-loading error

--- backend/app.js
+++ backend/app.js
@@ -8,16 +8,14 @@ const path = require('path');
 const { celebrate, Joi, errors } = require('celebrate');
 
 const NotFoundedError = require('./errors/NotFoundedError');
-
-const { DATABASE_URL } = require('./utils/configuration');
-const { limiter } = require('./middleware/limiter');
 const auth = require('./middleware/auth');
 const error = require('./middleware/error');
+const notFound = require('./utils/constants');
+const { DATABASE_URL } = require('./utils/configuration');
+const { limiter } = require('./middleware/limiter');
 const { requestLogger, errorLogger } = require('./middleware/logger');
 const { login, createUser } = require('./controllers/userController');
 const { userRoute } = require('./routes');
-
-const notFound = require('./utils/constants');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -33,27 +31,23 @@ mongoose.connect(DATABASE_URL);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// connecting routes
-app.use(express.static(path.join(__dirname, 'frontend-build')));
-// app.get('*', (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.sendFile('index.html', {root: path.join(__dirname, '../frontend-build')});
+app.use(express.static(path.join(__dirname, '..', 'frontend-build')));
+// app.get('/', (req, res) => {
+//   res.send();
 // });
 
-// app.use(auth);
-// app.use('/users', userRoute);
+app.get('*', (req, res, next) => {
+  next(new NotFoundedError(notFound));
+});
 
 //to handle testing server crash - to be removed once project passed the review
-// app.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Server will crash now');
-//   }, 0);
-// });
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
 
 // errors handling
-// app.get('*', (req, res, next) => {
-//   next(new NotFoundedError(notFound));
-// });
 app.use(errorLogger); //enabling error logger
 app.use(errors()); //celebrate error handler
 app.use(error); //centralized error handler
