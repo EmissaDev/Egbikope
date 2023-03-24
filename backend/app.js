6cb6ff603ad211c92a4e21f97dd3a7764248a2ab
Emiline <emissadev@gmail.com>
2023-03-23 21:51:07 -0700

    Fixing error 404 on page load

--- backend/app.js
+++ backend/app.js
@@ -34,10 +34,9 @@ mongoose.connect(DATABASE_URL);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // connecting routes
-app.use(express.static(path.join(__dirname, 'public')));
-
+// app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res, next) => {
-  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  res.sendFile('index.html', {root: path.join(__dirname, '../frontend-build')});
 });
 
 // app.use(auth);
